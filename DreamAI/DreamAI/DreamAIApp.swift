20729413f623ddcc510a6bd0579b2747e1d8ae23
shakhzodsunnatov <shaxzodsunnatov23@gmail.com>
2025-06-30 21:21:05 +0500

    Push fixed

--- DreamAI/DreamAI/DreamAIApp.swift
+++ DreamAI/DreamAI/DreamAIApp.swift
@@ -17,23 +17,26 @@ struct DreamAIApp: App {
     
     var body: some Scene {
         WindowGroup {
-            if authManager.isAuthenticated {
-                if authManager.hasCompletedPermissions {
-                    MainView()
-                        .environmentObject(subscriptionViewModel)
-                        .fullScreenCover(isPresented: $subscriptionViewModel.paywallIsPresent) {
-                            PaywallView()
-                        }
-                } else {
-                    NavigationStack {
-                        PermissionContainerView()
-                    }
-                }
-            } else {
-                NavigationStack {
-                    IntroView()
-                }
+            NavigationStack {
+                PermissionsSettingsUI()
             }
+//            if authManager.isAuthenticated {
+//                if authManager.hasCompletedPermissions {
+//                    MainView()
+//                        .environmentObject(subscriptionViewModel)
+//                        .fullScreenCover(isPresented: $subscriptionViewModel.paywallIsPresent) {
+//                            PaywallView()
+//                        }
+//                } else {
+//                    NavigationStack {
+//                        PermissionContainerView()
+//                    }
+//                }
+//            } else {
+//                NavigationStack {
+//                    IntroView()
+//                }
+//            }
         }
     }
 }
